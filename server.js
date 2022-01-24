160318e424e5e2311a72ffec2418ca6483664634
Himanshu Pal <himanshu.p19@iiits.in>
2022-01-25 00:36:21 +0530

    cors for a specific url

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://127.0.0.1:5500",
+  })
+);
+
+app.get("/data", (req, res) => {
+  res.json({ name: "Himanshu", favoirteFood: "Burger" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`server started at Port: ${PORT}`));
